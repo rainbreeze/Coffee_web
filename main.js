a19ee924a932a7e5536e21625ef480e583ce9d46
rainbreeze <rainbreeze2@naver.com>
2024-08-23 14:44:58 +0900

    헤더 완료

--- main.js
+++ main.js
@@ -14,3 +14,19 @@ searchInputEl.addEventListener('blur', function() {
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder', '');
 });
+
+const badgeE1 = document.querySelector('header .badges');
+
+window.addEventListener('scroll', function() {
+    console.log(window.scrollY);
+    if (window.scrollY > 500) {
+        gsap.to(badgeE1, 1, {
+            opacity: 0
+        });
+    }
+    else{
+        gsap.to(badgeE1, 1, {
+            opacity: 1
+        });
+    }
+});
