6d0c7de894586020a804d77c32eb8ef8d5fc224d
rainbreeze <rainbreeze2@naver.com>
2024-08-27 14:42:29 +0900

    add animation

--- youtube.js
+++ youtube.js
@@ -18,3 +18,15 @@
           }
         });
       }
+
+      const spyEls = document.querySelectorAll('section.scroll-spy');
+
+      spyEls.forEach(function(spyEl){
+        new ScrollMagic
+        .Scene({
+          triggerElement: spyEl,
+          triggerHook: .8
+        })
+        .setClassToggle(spyEl, 'show')
+        .addTo(new ScrollMagic.Controller());
+      });
